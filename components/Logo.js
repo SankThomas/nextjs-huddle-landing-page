8f2e94feec5c17e5cb81e3327a3046f7cc1f320c
Thomas Sankara <thomassankara74@gmail.com>
2021-04-08 17:27:53 +0300

    logo

---
+++ components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "../styles/Home.module.css"
+
+const Logo = () => {
+  return (
+    <>
+      <div className={styles.logo}>
+        <img src="/images/logo.svg" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default Logo
